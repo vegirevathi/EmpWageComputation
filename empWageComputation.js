57fed51813f1783a991ac242d09f5b972ac95ef8
vegirevathi <vegirevathi@gmail.com>
2020-08-03 07:03:31 +0530

    ADDED: Code for part time employee working hours and wage

--- empWageComputation.js
+++ empWageComputation.js
@@ -1,16 +1,22 @@
 console.log("Welcome to employee wage computation");
 
 let EMP_RATE_PER_HOUR = 20;
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 
 let empHrs = 0;
 let empWage = 0;
-let isPresent = 1;
 
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == isPresent)
+let empCheck = Math.floor(Math.random() * 10) % 3;
+if (empCheck == IS_PART_TIME)
 {
 	console.log("Employee is Present");
 	empHrs = 8;
+}
+else if (empCheck == IS_FULL_TIME)
+{
+	console.log("Employee is Part Time Present");
+	empHrs = 4;
 }
 else
 {
@@ -18,4 +24,4 @@ else
 	empHrs = 0;
 }
 empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log("Employee wage is" +empWage);
+console.log("Employee wage is " +empWage);
