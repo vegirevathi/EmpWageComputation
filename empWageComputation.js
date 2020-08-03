c97e0df0da053bf432770b321924a354c149acb3
vegirevathi <vegirevathi@gmail.com>
2020-08-03 06:45:25 +0530

    ADDED: Random check for employee status

--- empWageComputation.js
+++ empWageComputation.js
@@ -1,1 +1,8 @@
 console.log("Welcome to employee wage computation");
+let isPresent = 1;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == 1)
+	console.log("Employee is Present");
+else
+	console.log("Employee is Absent");
+
